a9b05fce067cb35339f1c653b45d5a2fa799ec34
user <user@DESKTOP-ONTMPNO.mshome.net>
2024-10-26 21:37:18 +0200

    sdfsadf1

---
+++ src/main/java/com/mycompany/elsoproba/Elsoproba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.elsoproba;
+
+/**
+ *
+ * @author user
+ */
+public class Elsoproba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
